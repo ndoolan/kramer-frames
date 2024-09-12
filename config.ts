a3aaa81579f1c9a23b20723c9f4a174b696588c0
ndoolan <nickdoolan31@gmail.com>
2024-09-12 08:46:32 -0400

    env config update prod mode

--- config.ts
+++ config.ts
@@ -2,10 +2,21 @@ import dotenvFlow from 'dotenv-flow';
 
 dotenvFlow.config();
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 export const config = {
-  RestApiUrl:
-    process.env.KV_REST_API_URL || import.meta.env.VITE_KV_REST_API_URL,
-  token: process.env.OTHER_DEV_VAR || import.meta.env.VITE_KV_REST_API_TOKEN,
+  RestApiUrl: isProduction
+    ? process.env.KV_REST_API_URL
+    : process.env.KV_REST_API_URL || import.meta.env.VITE_KV_REST_API_URL,
+  token: isProduction
+    ? process.env.KV_REST_API_TOKEN
+    : process.env.KV_REST_API_TOKEN || import.meta.env.VITE_KV_REST_API_TOKEN,
 };
+
+// export const config = {
+//   RestApiUrl:
+//     process.env.KV_REST_API_URL || import.meta.env.VITE_KV_REST_API_URL,
+//   token: process.env.OTHER_DEV_VAR || import.meta.env.VITE_KV_REST_API_TOKEN,
+// };
 
 // token: import.meta.env.VITE_KV_REST_API_TOKEN,
